10d156f16120a3e327871bd7903685bd4ea80059
ANunoT <nuno.teixeira@internode.on.net>
2018-04-07 15:04:19 +1000

    update 15:40 07/04/2018

--- best.R
+++ best.R
@@ -21,14 +21,14 @@ badState <- "Invalid state!"
 badOutcome <- " Invalid outcome!"
 
 # set the function
-best <- function (HospitalCode, reqOutcome) {
+best <- function (stateCode, reqOutcome) {
         
-# we know it use outcome-of-care-measures.csv. Let's make sure it exists, if exists read into dataset otherwise error message and stop
+# we use outcome-of-care-measures.csv. Let's make sure it exists, if not exist error message and stop
         
         if (!file.exists("C:/Users/Nuno/Documents/R-Programming/rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv")) stop(noFile)
         
 # lets make sure that the required outcome is one of "heart attack, "heart failure" or "pneumonia", if not one of these then error message and stop.
-# the sting compare functions with the strength=2 allows for capitalisation of the required outcome and thus will not error on an required outcome
+# the string compare functions with the strength=2 allows for capitalisation of the required outcome and thus will not error on an outcome
 # that is capitalised
         
         if (stri_cmp_equiv(reqOutcome, "heart attack", strength=2) == FALSE && stri_cmp_equiv(reqOutcome,"heart failure", strength=2) == FALSE && 
@@ -36,13 +36,18 @@ best <- function (HospitalCode, reqOutcome) {
         
 # At this stage the data file exists and the required outcome is valid. Need to check if state code is correct, but need to load data from file first
 # to ensure correct state code. This is the last check and is done only now because there was point in loading data (and use memory) if file did not 
-# exist, and if # the requested outcome was invalid. for processing. Then check to ensure that two # character state code id correct.
-# Load data into data frame, check that state code is correct by given state code is in column 7 of the file.
+# exist, and if the requested outcome was invalid. Check to ensure that two character state code id correct. Load data into data frame.
 
 OutcomeData <- read.csv("C:/Users/Nuno/Documents/R-Programming/rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv")
 
+# lets get a list of unique state, then check if the state argument exists. The argument is forced to upper case, ensuring no issues with capitalisation.
 uniqueState <- unique(OutcomeData[,7])
 
-print(getState)
+        if (!toupper(stateCode) %in% uniqueState) stop(badState)
+
+
+## At this stage all ready pro processing. 
+
+return("ready for precessing")
        
 }
